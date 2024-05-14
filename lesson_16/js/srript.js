1c97667ac4cd0ffb3c05f3c562463b1260fa6c00
Aliaksandra <132088742+Kukuliaka@users.noreply.github.com>
2024-05-14 15:51:09 +0300

    Add files via upload

--- lesson_16/js/srript.js
+++ lesson_16/js/srript.js
@@ -5,3 +5,9 @@ document.addEventListener('click', function (e) {
 		document.documentElement.classList.toggle('menu-open');
 	}
 })
+document.addEventListener('click', function (e) {
+	const targetElement = e.target;
+	if (targetElement.closest('.link-phone')) {
+		document.documentElement.classList.toggle('phone-open');
+	}
+})
