10973aadd7d9c9806711355a73e726073f1ef797
Aliaksandra <132088742+Kukuliaka@users.noreply.github.com>
2024-06-04 21:20:16 +0300

    Add files via upload

---
+++ lesson_21/js/srript.js
@@ -0,0 +1,55 @@
+
+
+// const userName = 20
+// userName = "Іштван"
+
+let numOne = 20
+let numTwo = "20"
+
+let numSumm = numOne + numTwo
+console.log(numSumm)
+
+let userName = "Alexa"
+console.log(userName)
+console.log(`hello, ${userName}!`)
+userName = "Sasha it's me"
+console.log(userName)
+console.log(typeof userName)
+console.log(userName.length)
+console.log(userName[9])
+console.log(userName.slice(2, 8))
+userName = userName.replace('me', 'you')
+console.log(userName)
+let someNum = '25.35px'
+console.log(parseInt(someNum))
+console.log(userName.toLowerCase())
+let varOne = "10";
+let varTwo = "3";
+let varRes = varOne * varTwo;
+console.log(typeof varRes);
+console.log(varRes);
+let userAge = 40
+let user = 'admin'
+if (user === 'user') {
+	console.log(userAge)
+} else {
+	console.log('Error')
+}
+let someNumber
+console.log(someNumber)
+console.log(typeof someNumber)
+someNumber = 28.35
+console.log(someNumber)
+console.log(typeof someNumber)
+let someSum = userName + someNumber
+console.log(someSum)
+let newNum = userName / someNumber
+console.log(newNum)
+console.log(Math.floor(someNumber))
+console.log(Math.ceil(someNumber))
+console.log(Math.round(someNumber))
+let someSize = '24.8px'
+console.log(parseInt(someSize))
+console.log(parseFloat(someSize))
+someNumber = ++someNumber
+console.log(someNumber)
